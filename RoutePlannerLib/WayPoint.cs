f1063851bd5d76431b41fa72af7f84091003f491
Chubacca73 <weberchr@me.com>
2014-09-23 17:04:51 +0200

    Distance MEthode

--- RoutePlannerLib/WayPoint.cs
+++ RoutePlannerLib/WayPoint.cs
@@ -9,6 +9,7 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
     public class WayPoint
     {
+        const int RADIUS = 6371;
 
         public string Name { get; set; }
         public double Longitude { get; set; }
@@ -23,7 +24,14 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 
         public int Distance(WayPoint target) {
 
-            return 0;
+            double tmp1 = Math.Sin(Latitude) * Math.Sin(target.Latitude);
+            double tmp2 = Math.Cos(Latitude) * Math.Cos(target.Latitude);
+            double tmp3 = Math.Cos(Longitude - target.Longitude);
+            double tmp4 = tmp2 * tmp3;
+
+            double d = 0;
+            d = RADIUS * Math.Acos(tmp1 + tmp4);
+            return (int)d;
         }
 
         public override string ToString()
