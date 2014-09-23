a6921ff85ad29bf74afdab9a1e333e55829d0639
mjenny <manuel.jenny@hispeed.ch>
2014-09-23 14:57:30 +0200

    City.cs constructor

---
+++ RoutePlannerLib/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    class City
+    {
+        string _name;
+        string _country;
+        int _population;
+        WayPoint _location;
+        public City(string Name, string Country, int Population, double Latitude, double Longitude)
+        {
+            _name = Name;
+            _country = Country;
+            _population = Population;
+            _location = new WayPoint(_name, Latitude, Longitude);
+        }
+    }
+}
