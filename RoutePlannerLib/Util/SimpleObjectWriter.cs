b3b00e021a0ea994b6960ad4af44f3036b81d648
Chubacca73 <weberchr@me.com>
2014-10-14 14:28:56 +0200

    Test added and Classes for Reader Writer

---
+++ RoutePlannerLib/Util/SimpleObjectWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
+{
+    public class SimpleObjectWriter
+    {
+        private System.IO.StringWriter stream;
+
+        public SimpleObjectWriter(System.IO.StringWriter stream)
+        {
+            // TODO: Complete member initialization
+            this.stream = stream;
+        }
+
+        public void Next(City c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
