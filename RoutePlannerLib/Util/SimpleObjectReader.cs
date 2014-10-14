f1a0e22c27687a02d5e4964db5ebada1cbcad57f
Chubacca73 <weberchr@me.com>
2014-10-14 14:32:53 +0200

    Method Stub in Reader Class

--- RoutePlannerLib/Util/SimpleObjectReader.cs
+++ RoutePlannerLib/Util/SimpleObjectReader.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
 {
-    class SimpleObjectReader
+    public class SimpleObjectReader
     {
+        private System.IO.StringReader stream;
+
+        public SimpleObjectReader(System.IO.StringReader stream)
+        {
+            // TODO: Complete member initialization
+            this.stream = stream;
+        }
+
+        public City Next()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
