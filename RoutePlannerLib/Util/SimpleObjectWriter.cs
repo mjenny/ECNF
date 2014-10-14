962d4484828d36ffa9b69e466302b8543691a5a3
Chubacca73 <weberchr@me.com>
2014-10-14 20:39:20 +0200

    Strings in LabTest corrected; Wrong namespace requested dcnf instead of ecnf Serialize Writer implemented

--- RoutePlannerLib/Util/SimpleObjectWriter.cs
+++ RoutePlannerLib/Util/SimpleObjectWriter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
 {
@@ -18,7 +19,36 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
 
         public void Next(City c)
         {
-            throw new NotImplementedException();
+            WriteType(c.GetType(), c);
+        }
+
+        private void WriteType(Type type, Object obj)
+        {
+            stream.WriteLine("Instance of {0}", type.FullName);
+
+            PropertyInfo[] props = type.GetProperties();
+
+            foreach (PropertyInfo item in type.GetProperties())
+            {
+                if ((item.PropertyType == typeof(System.String)))
+                {
+                    stream.WriteLine("{0}=\"{1}\"", item.Name, item.GetValue(obj));
+                }
+                else if ((item.PropertyType == typeof(System.Double)) ||
+                    (item.PropertyType == typeof(System.Int16)) ||
+                    (item.PropertyType == typeof(System.Int32)))
+                {
+                    stream.WriteLine("{0}={1}", item.Name, item.GetValue(obj));
+       
+                }
+                else if (item.PropertyType.BaseType == typeof(System.Object))
+                {
+                    stream.WriteLine("{0} is a nested object...", item.Name);
+                    object propValue = item.GetValue(obj);
+                    WriteType(Type.GetType(item.PropertyType.FullName), propValue);
+                }
+            }
+            stream.WriteLine("End of instance");
         }
     }
 }
