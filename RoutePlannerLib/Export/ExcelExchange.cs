11511d3c8d5d44d697c7524dbf26e7648a60159e
mjenny <manuel.jenny@hispeed.ch>
2014-10-28 15:55:54 +0100

    Implement Export.WriteToFile - Missing: Border Style

--- RoutePlannerLib/Export/ExcelExchange.cs
+++ RoutePlannerLib/Export/ExcelExchange.cs
@@ -17,8 +17,25 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Export
             Workbook excelWorkBook = excelApplication.Workbooks.Add(misValue);
             Worksheet excelWorkSheet = excelWorkBook.Worksheets.get_Item(1);
 
-            excelWorkSheet.Cells[1, 1] = "Hallo Excel";
-            excelWorkBook.SaveAs("TestFile.xlst");
+            excelWorkSheet.Cells[1, 1] = "From";
+            excelWorkSheet.Cells[1, 2] = "To";
+            excelWorkSheet.Cells[1, 3] = "Distance";
+            excelWorkSheet.Cells[1, 3] = "Transport Mode";
+            for (int i = 1; i < 5; i++)
+            {
+                excelWorkSheet.Cells[1, i].Font.Size = 14;
+            }
+
+            int j = 2;
+            foreach (var l in links)
+            {
+                excelWorkSheet.Cells[j, 1] = l.FromCity.Name + " (" + l.FromCity.Country + ")";
+                excelWorkSheet.Cells[j, 2] = l.ToCity.Name + " (" + l.ToCity.Country + ")";
+                excelWorkSheet.Cells[j, 3] = l.Distance;
+                excelWorkSheet.Cells[j, 4] = l.TransportMode.ToString();
+                ++j;
+            }
+            excelWorkBook.SaveAs(fileName);
             excelWorkBook.Close();
             return "Ok";
 
