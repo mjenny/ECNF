010bd6524ed8e185314790bd387293694057d655
Chubacca73 <weberchr@me.com>
2014-10-28 14:58:08 +0100

    Dynami (Aufgabe2) done

---
+++ RoutePlannerLib/Dynamic/World.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Dynamic
+{
+    public class World : DynamicObject
+    {
+        private Cities _cities;
+        public World(Cities cities)
+        {
+            _cities = cities;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            Console.WriteLine("Tryed to invoke: {0}", binder.Name);
+            var city = _cities.FindCity(binder.Name);
+            if (city != null)
+            {
+                result = city;
+            }
+            else
+            {
+                result = "The city \"" + binder.Name + "\" does not exist!";
+            }
+            
+            return true;
+        }
+
+    }
+}
---
+++ RoutePlannerLib/Export/ExcelExchange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Export
+{
+    public class ExcelExchange
+    {
+ 
+        public string WriteToFile(String fileName, City from, City to, List<Link> links)
+        {
+            object misValue = System.Reflection.Missing.Value; 
+            Application excelApplication = new Application();
+            Workbook excelWorkBook = excelApplication.Workbooks.Add(misValue);
+            Worksheet excelWorkSheet = excelWorkBook.Worksheets.get_Item(1);
+
+            excelWorkSheet.Cells[1, 1] = "Hallo Excel";
+            excelWorkBook.SaveAs("TestFile.xlst");
+            excelWorkBook.Close();
+            return "Ok";
+
+        }
+    }
+}
